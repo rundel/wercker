18b30b67e65efea86ee4695ba2340564e788f0ab
Colin Rundel <rundel@gmail.com>
2019-05-23 14:10:39 +0100

    Update docs

--- R/wercker.R
+++ R/wercker.R
@@ -8,10 +8,16 @@
 #' * [`set_wercker_token`] - set wercker token
 #' * [`test_wercker_token`] - test wercker token
 #'
-#' @section apps:
+#' @section wercker applications:
 #'
-#' * [``] -
-#' * [``] -
+#' * [`add_wercker`] - add wercker to github repo
+#' * [`check_wercker`] - check that= wercker app(s) have been created
+#'
+#' @section wercker user:
+#'
+#' * [`get_wercker_orgs`] - returns details on user's organizations
+#' * [`get_wercker_apps`] - returns details on user's / organization's applications
+#' * [`create_wercker_org`] - creates wercker organization(s)
 #'
 #' @section badges:
 #'
